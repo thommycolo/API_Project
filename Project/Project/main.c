a637d0f717aac13354124e81e2f47cf44e5b1053
ThommyColo <thommy.colombo@gmail.com>
2024-07-23 12:28:33 +0200

    starting

--- Project/Project/main.c
+++ Project/Project/main.c
@@ -1,6 +1,15 @@
-#include <Stdio.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-int maiun() {
 
-	pritnf("Bebbo il cudissimo\n");
+
+int main() {
+
+	int clock_corr, dim_corr;
+
+	scanf(" %d %d ", &clock_corr, &dim_corr);
+	
+
+
 }
